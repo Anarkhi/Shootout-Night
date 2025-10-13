f22de4bccc646cd896e3e61c60d01018bd263bc5
Dennys Quintanilha <33529561+Anarkhi@users.noreply.github.com>
2025-10-13 14:30:40 -0300

    Revert "refact: refactoring codebase (#8)"
    This reverts commit 3c7f306c481255f4723d33d3c13d08e0480eb2cd.

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import random
 import asyncio
 import json
 
-# Local imports
-import entities
-
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -13,31 +10,6 @@ pygame.display.set_caption("Shootout Night")
 icon_image = pygame.image.load("Assets/ShootoutNightIcon.png")
 pygame.display.set_icon(icon_image)
 clock = pygame.time.Clock()
-
-def redrawGameScreen(protagonist, player_pos, bullets, enemies, score, hightScore, threat):
-        screen.fill("black")
-        show_score(score, hightScore, threat)
-        protagonist.draw(player_pos)
-        for bullet in bullets:
-            bullet.draw(screen)
-        for thug in enemies:
-            thug.draw(threat, screen)
-        pygame.display.flip()
-
-def show_score(score, hightScore, threat):
-    font_obj = pygame.font.SysFont('comicsans',50,True)
-    score_txt = font_obj.render("Score: " +str(score) ,1, (255,255,255))
-    hightScore_txt = font_obj.render("Highest Score: " +str(hightScore) ,1, (255,255,255))
-    threat_txt = font_obj.render("Threat: " +str(threat) ,1, (255,255,255))
-    screen.blit(score_txt,(20,20))
-    screen.blit(hightScore_txt,(20,60))
-    screen.blit(threat_txt,(1050,20))
-
-def save_game(score, hightScore):
-    if score > hightScore:
-        game_data = {"score": score}
-        with open("Assets/savegame.json",'w') as file:
-            file.write(json.dumps(game_data))
 
 async def game():
     dt = 0
@@ -57,6 +29,73 @@ async def game():
     player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
     enemy_pos = pygame.Vector2(0,0)
 
+    class player(object):
+            def __init__(self, x, y, width, height):
+                self.x = x
+                self.y = y
+                self.width = width
+                self.height = height
+                self.speed = 200
+            def draw(self,player_pos):
+                pygame.draw.circle(screen, "white", player_pos, 15)
+
+    class projectile(object):
+        def __init__(self,x,y,spawnX,spawnY,radius,color,direction,party):
+            self.x = x
+            self.y = y
+            self.radius = radius
+            self.color = color
+            self.direction = direction
+            self.spawnX = spawnX
+            self.spawnY = spawnY
+            self.party = party
+
+        def draw(self,screen):
+            pygame.draw.circle(screen,self.color,(self.x,self.y),self.radius)
+
+    class enemy(object):
+        def __init__(self, x, y, radius, width, height, speed, targetStopX,targetStopY,thugShotCD=200):
+                self.x = x
+                self.y = y
+                self.radius = radius
+                self.width = width
+                self.height = height
+                self.targetStopX = targetStopX
+                self.targetStopY = targetStopY
+                self.speed = speed
+                self.thugShotCD = thugShotCD
+
+        def draw(self):
+            if threat != "MAX":
+                enemyColor = "gray"
+            else: enemyColor = "red"
+            pygame.draw.circle(screen, enemyColor, pygame.Vector2(self.x,self.y), self.radius)
+
+    def redrawGameScreen():
+        screen.fill("black")
+        show_score()
+        protagonist.draw(player_pos)
+        for bullet in bullets:
+            bullet.draw(screen)
+        for thug in enemies:
+            thug.draw()
+        pygame.display.flip()
+
+    def show_score():
+        font_obj = pygame.font.SysFont('comicsans',50,True)
+        score_txt = font_obj.render("Score: " +str(score) ,1, (255,255,255))
+        hightScore_txt = font_obj.render("Hight Score: " +str(hightScore) ,1, (255,255,255))
+        threat_txt = font_obj.render("Threat: " +str(threat) ,1, (255,255,255))
+        screen.blit(score_txt,(20,20))
+        screen.blit(hightScore_txt,(20,60))
+        screen.blit(threat_txt,(1050,20))
+
+    def save_game(score):
+        if score > hightScore:
+            game_data = {"score": score}
+            with open("Assets/savegame.json",'w') as file:
+                file.write(json.dumps(game_data))
+
     try:
         with open("Assets/savegame.json", 'r') as f:
             loaded_data = json.load(f)
@@ -66,7 +105,7 @@ async def game():
         print("No save game found.")
 
     # MainLoop
-    protagonist = entities.player(screen.get_width() / 2, screen.get_height() / 2,0,0, screen)
+    protagonist = player(screen.get_width() / 2, screen.get_height() / 2,0,0)
     running = True
     bullets = []
     enemies = []
@@ -101,7 +140,7 @@ async def game():
                         playerCollisionRect = pygame.Rect(player_pos.x-15,player_pos.y-15,30,30)
                         if bulletCollisionRect.colliderect(playerCollisionRect):
                             bullets.pop(bullets.index(bullet))
-                            save_game(score, hightScore)
+                            save_game(score)
                             running = False
             else: bullets.pop(bullets.index(bullet))
 
@@ -133,32 +172,32 @@ async def game():
             enemySpawnSide = random.randint(1, 4)
             match enemySpawnSide:
                 case 1:
-                    enemies.append(entities.enemy(random.randint(50, 1230), -50,15,15,15,1,random.randint(50, 1230),50))
+                    enemies.append(enemy(random.randint(50, 1230), -50,15,15,15,1,random.randint(50, 1230),50))
                 case 2:
-                    enemies.append(entities.enemy(random.randint(50, 1230), 770,15,15,15,1,random.randint(50, 1230), 670))
+                    enemies.append(enemy(random.randint(50, 1230), 770,15,15,15,1,random.randint(50, 1230), 670))
                 case 3:
-                    enemies.append(entities.enemy(-50, random.randint(50,670),15,15,15, 1, 50,random.randint(50,670)))
+                    enemies.append(enemy(-50, random.randint(50,670),15,15,15, 1, 50,random.randint(50,670)))
                 case 4:
-                    enemies.append(entities.enemy(1330, random.randint(50, 670),15,15,15,1, 1230,random.randint(50, 670)))
+                    enemies.append(enemy(1330, random.randint(50, 670),15,15,15,1, 1230,random.randint(50, 670)))
             enemySpawnCD = 150*threatMult
         elif threat == "MAX":
             enemySpawnSide = random.randint(1, 4)
             match enemySpawnSide:
                 case 1:
-                    enemies.append(entities.enemy(random.randint(50, 1230), -50,15,15,15, 3,random.randint(50, 1230),random.randint(50, 670)))
+                    enemies.append(enemy(random.randint(50, 1230), -50,15,15,15, 3,random.randint(50, 1230),random.randint(50, 670)))
                 case 2:
-                    enemies.append(entities.enemy(random.randint(50, 1230), 770,15,15,15, 3,random.randint(50, 1230), random.randint(50, 670)))
+                    enemies.append(enemy(random.randint(50, 1230), 770,15,15,15, 3,random.randint(50, 1230), random.randint(50, 670)))
                 case 3:
-                    enemies.append(entities.enemy(-50, random.randint(50,670),15,15,15, 3,random.randint(50, 1230), random.randint(50,670)))
+                    enemies.append(enemy(-50, random.randint(50,670),15,15,15, 3,random.randint(50, 1230), random.randint(50,670)))
                 case 4:
-                    enemies.append(entities.enemy(1330, random.randint(50, 670),15,15,15, 3,random.randint(50, 1230), random.randint(50, 670)))
+                    enemies.append(enemy(1330, random.randint(50, 670),15,15,15, 3,random.randint(50, 1230), random.randint(50, 670)))
             enemySpawnCD = 150*threatMult
 
         #Inimigos Andando e Disparando
         for thug in enemies:
             thug.thugShotCD -=1
             if thug.thugShotCD == 0:
-                bullets.append(entities.projectile(round(thug.x),round(thug.y),round(thug.x),round(thug.y),3,"gray",pygame.Vector2(player_pos.x,player_pos.y),2))
+                bullets.append(projectile(round(thug.x),round(thug.y),round(thug.x),round(thug.y),3,"gray",pygame.Vector2(player_pos.x,player_pos.y),2))
                 thug.thugShotCD = 100
             if (thug.x < thug.targetStopX):
                 thug.x +=thug.speed
@@ -177,13 +216,13 @@ async def game():
             shotCD -= 1
         else:
             if keysPress[pygame.K_LEFT]:
-                bullets.append(entities.projectile(round(player_pos.x-6),round(player_pos.y),round(player_pos.x-6),round(player_pos.y),3,"white",-pygame.Vector2(1,0),1))
+                bullets.append(projectile(round(player_pos.x-6),round(player_pos.y),round(player_pos.x-6),round(player_pos.y),3,"white",-pygame.Vector2(1,0),1))
             elif keysPress[pygame.K_RIGHT]:
-                bullets.append(entities.projectile(round(player_pos.x+6),round(player_pos.y),round(player_pos.x+6),round(player_pos.y),3,"white",pygame.Vector2(1,0),1))
+                bullets.append(projectile(round(player_pos.x+6),round(player_pos.y),round(player_pos.x+6),round(player_pos.y),3,"white",pygame.Vector2(1,0),1))
             elif keysPress[pygame.K_UP]:
-                bullets.append(entities.projectile(round(player_pos.x),round(player_pos.y-6),round(player_pos.x),round(player_pos.y-6),3,"white",-pygame.Vector2(0,1),1))
+                bullets.append(projectile(round(player_pos.x),round(player_pos.y-6),round(player_pos.x),round(player_pos.y-6),3,"white",-pygame.Vector2(0,1),1))
             elif keysPress[pygame.K_DOWN]:
-                bullets.append(entities.projectile(round(player_pos.x),round(player_pos.y+6),round(player_pos.x),round(player_pos.y+6),3,"white",pygame.Vector2(0,1),1))
+                bullets.append(projectile(round(player_pos.x),round(player_pos.y+6),round(player_pos.x),round(player_pos.y+6),3,"white",pygame.Vector2(0,1),1))
             shotCD = 40
 
 
@@ -219,7 +258,7 @@ async def game():
         # dt is delta time in seconds since last frame, used for framerate-
         # independent physics.
         dt = clock.tick(60) / baseTime
-        redrawGameScreen(protagonist, player_pos, bullets, enemies, score, hightScore, threat)
+        redrawGameScreen()
         await asyncio.sleep(0)  # Yield control to the event loop
 
     pygame.quit()
